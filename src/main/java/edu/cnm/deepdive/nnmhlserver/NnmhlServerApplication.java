b7f417b2d3ec3c2ad7f941bed1ee45f87946723a
CodeCanna <codecannamw@gmail.com>
2021-10-24 10:10:29 -0600

    Commit initial

---
+++ src/main/java/edu/cnm/deepdive/nnmhlserver/NnmhlServerApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.nnmhlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnmhlServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NnmhlServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/nnmhlserver/NnmhlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.nnmhlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnmhlServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
