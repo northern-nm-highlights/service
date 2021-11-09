17dc5068720fd695fb7bc2dddb57fa04d6984f52
nmitchell313 <nmitchell11@cnm.edu>
2021-11-09 16:26:39 -0700

    UserRepository, UserService, pom, application updated.

---
+++ src/main/java/edu/cnm/deepdive/nnmhlserver/model/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.nnmhlserver.model.dao;
+
+import edu.cnm.deepdive.nnmhlserver.model.entity.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByOauthKey(String oauthKey);
+
+  Optional<User> findByExternalKey(UUID externalKey);
+}
